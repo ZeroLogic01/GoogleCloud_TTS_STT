1ffd58437c2e7b82ed8e0b82c6f9cfa66ad2e137
Muhammad Nabeel <61131901+ZeroLogic01@users.noreply.github.com>
2020-07-06 09:17:05 +0500

    Simple child window is working.
    Complied SimpleChilWindow package with latest version of the mahapps.metro separately then included it as a reference.

--- Modules/GoogleCloud_TTS_STT.Modules.TextToSpeech/SSML/AddBreak.xaml.cs
+++ Modules/GoogleCloud_TTS_STT.Modules.TextToSpeech/SSML/AddBreak.xaml.cs
@@ -35,5 +35,6 @@ namespace GoogleCloud_TTS_STT.Modules.TextToSpeech.SSML
         {
             this.Close(false);
         }
+
     }
 }
--- Modules/GoogleCloud_TTS_STT.Modules.TextToSpeech/Views/MainView.xaml.cs
+++ Modules/GoogleCloud_TTS_STT.Modules.TextToSpeech/Views/MainView.xaml.cs
@@ -1,21 +1,8 @@
 ﻿using GoogleCloud_TTS_STT.Modules.TextToSpeech.SSML;
-using GoogleCloud_TTS_STT.Modules.TextToSpeech.ViewModels;
-using MahApps.Metro.Controls.Dialogs;
+using MahApps.Metro.Controls;
 using MahApps.Metro.SimpleChildWindow;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GoogleCloud_TTS_STT.Modules.TextToSpeech.Views
 {
@@ -31,7 +18,7 @@ namespace GoogleCloud_TTS_STT.Modules.TextToSpeech.Views
         }
         private async void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
-            await Application.Current.MainWindow.ShowChildWindowAsync<bool>(new AddBreak() { IsModal = true, AllowMove = true }, ChildWindowManager.OverlayFillBehavior.FullWindow);
+            await ((MetroWindow)Window.GetWindow(this)).ShowChildWindowAsync<bool>(new AddBreak() { IsModal = true, AllowMove = true }, ChildWindowManager.OverlayFillBehavior.WindowContent);
 
         }
     }
