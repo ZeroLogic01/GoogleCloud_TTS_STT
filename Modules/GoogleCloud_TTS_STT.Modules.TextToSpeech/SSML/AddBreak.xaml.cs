cc1bf14524168e899ebaf9665742ad4922462595
Muhammad Nabeel <61131901+ZeroLogic01@users.noreply.github.com>
2020-07-06 08:41:53 +0500

    SSML break tag

---
+++ Modules/GoogleCloud_TTS_STT.Modules.TextToSpeech/SSML/AddBreak.xaml.cs
@@ -0,0 +1,39 @@
+﻿using MahApps.Metro.SimpleChildWindow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GoogleCloud_TTS_STT.Modules.TextToSpeech.SSML
+{
+    /// <summary>
+    /// Interaction logic for AddBreak.xaml
+    /// </summary>
+    public partial class AddBreak : ChildWindow
+    {
+        public AddBreak()
+        {
+            InitializeComponent();
+        }
+
+        private void OkButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            this.Close(true);
+        }
+
+        private void CloseSec_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Close(false);
+        }
+    }
+}
